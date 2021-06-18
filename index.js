17c2f82829e6cab4eaad16e6608126da33688305
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-18 13:44:58 -0400

    updated gitignore, started working through required inquirer prompts in index.js

--- index.js
+++ index.js
@@ -2,5 +2,39 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+const fs = require("fs");
+const inquirer = require('inquirer')
+
+//prompt questions
+
+const managerQuestions = [
+    {
+        type:'input',
+        message: 'What is the managers name?',
+        name:'managerName'
+    },
+    {
+        type:'input',
+        message: 'What is the managers id?',
+        name:'managerId'
+    },
+    {
+        type:'input',
+        message: 'What is the managers email?',
+        name:'managerEmail'
+    },
+    {
+        type:'input',
+        message: 'What is the managers office number?',
+        name:'managerOfficeNumber'
+    }
+];
 
 
+
+const init = () => {
+    inquirer
+        .prompt(managerQuestions)
+}
+
+init();
