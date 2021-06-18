6457538eca5fe5d3d98a763f2a6014c1f90b4b8b
mkotte <74936870+mkotte@users.noreply.github.com>
2021-06-18 18:30:45 -0400

    furthered functions for index.js

--- index.js
+++ index.js
@@ -30,13 +30,59 @@ const managerQuestions = [
     }
 ];
 
-const generateNewTeam = () => {
-
-}
+const generateNewTeam = (data) => {
+    fs.writeFile('.dist/index.html', 
+    `<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <link
+                rel="stylesheet"
+                href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                />
+            </head>
+            <body>
+                <main class="jumbotron">
+                    <div class="col-3">
+                        <div class="card">
+                            <div class="card-header">
+                                <h2>${data.name}</h2>
+                            </div>
+                            <div class="card-body">
+                                <ul>
+                                    <li>ID: ${data.id}</li>
+                                    <li>Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                                    <li>Office number: ${data.officeNumber}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+        `
+    )
+};
 
 const addNextEmployee = () => {
-    
-}
+    inquirer 
+        .prompt(employeeQs)
+        .then((answers) =>{
+            switch (answers.role) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    fs.appendFile('.dist/index.html', `
+                            </section>
+                        </body>
+                    </html>`
+                    , (err) => {if (err) {throw err} else console.log('Successfully appended!')}) ;
+            }
+        })
+};
 
 const init = () => {
     inquirer
@@ -45,7 +91,7 @@ const init = () => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
             generateNewTeam(manager);
             addNextEmployee();
-        })
-}
+        });
+};
 
 init();
